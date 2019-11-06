3a1a1043548855ad5dfd05921b945c292adc1196
Sudhir Jonathan <sudhir.j@gmail.com>
2019-11-06 10:29:08 +0530

    Simplified example and README

--- cirque_test.go
+++ cirque_test.go
@@ -12,7 +12,7 @@ import (
 func TestCirque(t *testing.T) {
 	inputs := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
 	expectedOutput := []int{2, 4, 6, 8, 10, 12, 14, 16, 18, 20}
-	done := make(chan bool)
+
 	var measuredParallelism int64 = 0
 
 	var maxParallelism int64 = 3
@@ -24,18 +24,6 @@ func TestCirque(t *testing.T) {
 	})
 
 	go func() {
-		var actualOutput []int
-		for i := range outputChannel {
-			actualOutput = append(actualOutput, i.(int))
-		}
-		if !reflect.DeepEqual(expectedOutput, actualOutput) {
-			t.Error("WRONG WRONG WRONG")
-			t.Log(expectedOutput)
-			t.Log(actualOutput)
-		}
-		done <- true
-	}()
-	go func() {
 		for _, i := range inputs {
 			inputChannel <- i
 			if atomic.LoadInt64(&measuredParallelism) > maxParallelism {
@@ -45,13 +33,20 @@ func TestCirque(t *testing.T) {
 		close(inputChannel)
 	}()
 
-	<-done
+	var actualOutput []int
+	for i := range outputChannel {
+		actualOutput = append(actualOutput, i.(int))
+	}
+	if !reflect.DeepEqual(expectedOutput, actualOutput) {
+		t.Error("WRONG WRONG WRONG")
+		t.Log(expectedOutput)
+		t.Log(actualOutput)
+	}
 }
 
 func ExampleNewCirque() {
 	inputs := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
 
-	done := make(chan bool)
 	inputChannel, outputChannel := NewCirque(3, func(i interface{}) interface{} {
 		time.Sleep(time.Duration(rand.Int63n(100)) * time.Millisecond)
 		return i.(int) * 2
@@ -64,15 +59,11 @@ func ExampleNewCirque() {
 		close(inputChannel)
 	}()
 
-	go func() {
-		var actualOutput []int
-		for i := range outputChannel {
-			actualOutput = append(actualOutput, i.(int))
-		}
-		fmt.Println(actualOutput)
-		done <- true
-	}()
+	var output []int
+	for i := range outputChannel {
+		output = append(output, i.(int))
+	}
+	fmt.Println(output)
 
-	<-done
 	// Output: [2 4 6 8 10 12 14 16 18 20]
 }
