74b898467fdc197562bfe7c27fa55390ee2dbdce
Sudhir Jonathan <sudhir.j@gmail.com>
2019-11-22 15:22:12 +0530

    Full rewrite using much simple worker pools and channels

--- cirque.go
+++ cirque.go
@@ -2,14 +2,17 @@ package cirque
 
 import (
 	"sync"
-	"sync/atomic"
-	"time"
 )
+
+type indexedValue struct {
+	value interface{}
+	index int64
+}
 
 // NewCirque creates a FIFO parallel queue that runs a given processor function on each job, similar to a parallel Map.
 //
 // The method accepts a parallelism number, which the maximum number of jobs that are processed simultaneously,
-// and a processor function that takes a job as input and returns a result as output. The processor function must be safe
+// and a processor function that takes a job as input and returns a indexedValue as output. The processor function must be safe
 // to call from multiple goroutines.
 //
 // It returns two channels, one into which inputs can be passed, and one from which outputs can be read.
@@ -18,75 +21,57 @@ func NewCirque(parallelism int64, processor func(interface{}) interface{}) (chan
 	input := make(chan interface{})
 	output := make(chan interface{})
 
-	stagedResults := sync.Map{}
-	popSignal := sync.NewCond(&sync.Mutex{})
-	pushSignal := sync.NewCond(&sync.Mutex{})
+	processedJobs := make(chan indexedValue)
+	go func() { // process inputs
+		wg := sync.WaitGroup{}
+		splitter := make(chan indexedValue)
 
-	var completeFlag int64 = 0 // ideally a boolean, but we want an atomic variable without typecasts
-	var leadingIndex int64 = 0
-	var followingIndex int64 = 0
-	processingFinished := make(chan struct{})
+		// Start worker pool of specified size
+		for workerID := int64(0); workerID < parallelism; workerID++ {
+			go func() {
+				for job := range splitter {
+					processedJobs <- indexedValue{
+						value: processor(job.value),
+						index: job.index,
+					}
+					wg.Done()
+				}
+			}()
+		}
 
-	go func() { // process all the inputs
-		popSignal.L.Lock()
-		defer popSignal.L.Unlock()
-
+		index := int64(0)
 		for job := range input {
-			for (aR(&leadingIndex) - aR(&followingIndex)) >= parallelism {
-				popSignal.Wait() // parallelism limit reached, wait until a pop happens
+			wg.Add(1)
+			splitter <- indexedValue{
+				value: job,
+				index: index,
 			}
-			go func(i int64, j interface{}) {
-				stagedResults.Store(i, processor(j))
-				pushSignal.Broadcast() // store the result and broadcast a push event
-			}(leadingIndex, job)
-			aInc(&leadingIndex)
+			index = index + 1
 		}
-		pushSignal.Broadcast() // when input channel is empty, broadcast push event to close output channel
-		aInc(&completeFlag)
-		// This is to avoid race condition where output channel does not close.
-		for {
-			select {
-			case <-processingFinished:
-				return
+		close(splitter)
 
-			case <-time.After(10 * time.Millisecond):
-				pushSignal.Broadcast()
-			}
-		}
+		wg.Wait()
+		close(processedJobs)
 	}()
 
 	go func() { // send outputs in order
-		pushSignal.L.Lock()
-		defer pushSignal.L.Unlock()
-
-		for { // infinite loop that relies on the push event wait for holding
-			if aR(&completeFlag) > 0 && aR(&followingIndex) == aR(&leadingIndex) {
-				// all inputs have been accepted and we've caught up on outputs
-				close(output)
-				processingFinished <- struct{}{}
-				return
-			}
-
-			if value, ok := stagedResults.Load(followingIndex); ok {
-				output <- value
-				go stagedResults.Delete(followingIndex) // clear the map behind us, keeps memory usage constant
-				go popSignal.Broadcast()
-				aInc(&followingIndex)
-			} else {
-				pushSignal.Wait()
+		nextIndex := int64(0)
+		storedResults := map[int64]indexedValue{}
+		for res := range processedJobs {
+			storedResults[res.index] = res
+			canSend := true
+			for canSend {
+				if storedResult, ok := storedResults[nextIndex]; ok {
+					output <- storedResult.value
+					delete(storedResults, storedResult.index)
+					nextIndex = nextIndex + 1
+				} else {
+					canSend = false
+				}
 			}
 		}
+		close(output)
 	}()
 
 	return input, output
-}
-
-// aInc atomically increments the given int
-func aInc(i *int64) int64 {
-	return atomic.AddInt64(i, 1)
-}
-
-// aR atomically reads the given int
-func aR(i *int64) int64 {
-	return atomic.LoadInt64(i)
 }
